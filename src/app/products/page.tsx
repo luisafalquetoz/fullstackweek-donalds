705cd2efeea19d1655e6c3927723599c16e7c1d6
luisafalquetoz <luisa_fzandonade@hotmail.com>
2025-02-18 17:53:24 -0300

    feat: add shadcn initial setup and components

---
+++ src/app/products/page.tsx
@@ -0,0 +1,14 @@
+import { Button} from '@/components/ui/button';
+import { Input } from '@/components/ui/input';
+
+const Products = () => {
+    return ( 
+        <div>
+            <h1>Products Page</h1>
+            <Button>CLIQUE AQUI</Button>
+            <Input placeholder='Escreva aqui' />
+        </div> 
+    );
+}
+
+export default Products;
