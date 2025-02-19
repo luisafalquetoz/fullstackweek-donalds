0f558af7fed2f37da57c8e78df7b3151b96b9566
luisafalquetoz <luisa_fzandonade@hotmail.com>
2025-02-19 18:27:17 -0300

    feat: add initial restaurant page and menu page

--- src/app/products/page.tsx
+++ src/app/products/page.tsx
@@ -1,7 +1,7 @@
 import { Button} from '@/components/ui/button';
 import { Input } from '@/components/ui/input';
 
-const Products = () => {
+const ProductsPage = () => {
     return ( 
         <div>
             <h1>Products Page</h1>
@@ -11,4 +11,4 @@ const Products = () => {
     );
 }
 
-export default Products;
+export default ProductsPage;
