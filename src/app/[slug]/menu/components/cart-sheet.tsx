6e1de848e43a7b01614a567ffe455d703f2bb4cd
luisafalquetoz <luisa_fzandonade@hotmail.com>
2025-02-20 19:16:13 -0300

    feat: add initial cart context and cart sheet component

---
+++ src/app/[slug]/menu/components/cart-sheet.tsx
@@ -0,0 +1,35 @@
+import {
+	Sheet,
+	SheetContent,
+	SheetDescription,
+	SheetHeader,
+	SheetTitle,
+} from '@/components/ui/sheet';
+import { useContext } from 'react';
+import { CartContext } from '../contexts/cart';
+
+const CartSheet = () => {
+    const { isOpen, toggleCart } = useContext(CartContext);
+
+    const handleAddToCart = () => {
+		toggleCart();
+	};
+
+    return (
+        <Sheet open={isOpen} onOpenChange={toggleCart}>
+				<SheetContent>
+					<SheetHeader>
+						<SheetTitle>Title</SheetTitle>
+						<SheetDescription>
+							Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nam
+							necessitatibus est a sequi aut quidem repellat ratione dignissimos
+							molestiae, officiis aliquid magni impedit doloribus praesentium,
+							consectetur eveniet ipsum repellendus facere!
+						</SheetDescription>
+					</SheetHeader>
+				</SheetContent>
+			</Sheet>
+    );
+}
+
+export default CartSheet;
