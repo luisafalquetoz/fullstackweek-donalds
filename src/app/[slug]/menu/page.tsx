44b49cb4ad7d371c11510472c0e5eb8b68ae1942
luisafalquetoz <luisa_fzandonade@hotmail.com>
2025-02-20 13:53:45 -0300

    chore: update biome as new linter

--- src/app/[slug]/menu/page.tsx
+++ src/app/[slug]/menu/page.tsx
@@ -1,47 +1,50 @@
-import { Button } from "@/components/ui/button";
-import { db } from "@/lib/prisma";
-import { ChevronLeftIcon, ScrollTextIcon } from "lucide-react";
+import { Button } from '@/components/ui/button';
+import { db } from '@/lib/prisma';
+import { ChevronLeftIcon, ScrollTextIcon } from 'lucide-react';
 
-import Image from "next/image";
-import { notFound } from "next/navigation";
-import RestaurantHeader from "./components/header";
-import RestaurantCategories from "./components/categories";
+import Image from 'next/image';
+import { notFound } from 'next/navigation';
+import RestaurantCategories from './components/categories';
+import RestaurantHeader from './components/header';
 
 interface RestaurantMenuPageProps {
-    params: Promise<{ slug: string }>;
-    searchParams: Promise<{ consumptionMethod: string }>;
+	params: Promise<{ slug: string }>;
+	searchParams: Promise<{ consumptionMethod: string }>;
 }
 
 const isConsumptionMethodValid = (consumptionMethod: string) => {
-    return ["DINE_IN", "TAKEAWAY"].includes(consumptionMethod.toUpperCase());
-}
+	return ['DINE_IN', 'TAKEAWAY'].includes(consumptionMethod.toUpperCase());
+};
 
-const RestaurantMenuPage = async ({params, searchParams}: RestaurantMenuPageProps) => {
-    const {slug} = await params;
-    const {consumptionMethod} = await searchParams;
-    if (!isConsumptionMethodValid(consumptionMethod)) {
-        return notFound();
-    }
-    const restaurant = await db.restaurant.findUnique({
-        where: {slug}, 
-        include: {
-            menuCategories: {
-                include: {
-                    products: true
-                }
-            },
-        },
-    });
-    if (!restaurant) {
-        return notFound();
-    }
+const RestaurantMenuPage = async ({
+	params,
+	searchParams,
+}: RestaurantMenuPageProps) => {
+	const { slug } = await params;
+	const { consumptionMethod } = await searchParams;
+	if (!isConsumptionMethodValid(consumptionMethod)) {
+		return notFound();
+	}
+	const restaurant = await db.restaurant.findUnique({
+		where: { slug },
+		include: {
+			menuCategories: {
+				include: {
+					products: true,
+				},
+			},
+		},
+	});
+	if (!restaurant) {
+		return notFound();
+	}
 
-    return (
-        <div> 
-            <RestaurantHeader restaurant={restaurant}/>
-            <RestaurantCategories restaurant={restaurant}/>
-        </div>
-    );
-}
+	return (
+		<div>
+			<RestaurantHeader restaurant={restaurant} />
+			<RestaurantCategories restaurant={restaurant} />
+		</div>
+	);
+};
 
 export default RestaurantMenuPage;
