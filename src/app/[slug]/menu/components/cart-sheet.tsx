c05c9f47a8abf97a1debfd12beb8d6d8bef7ecef
luisafalquetoz <luisa_fzandonade@hotmail.com>
2025-02-20 19:31:53 -0300

    feat: add initial function to add to cart context

--- src/app/[slug]/menu/components/cart-sheet.tsx
+++ src/app/[slug]/menu/components/cart-sheet.tsx
@@ -9,27 +9,32 @@ import { useContext } from 'react';
 import { CartContext } from '../contexts/cart';
 
 const CartSheet = () => {
-    const { isOpen, toggleCart } = useContext(CartContext);
+	const { isOpen, toggleCart, products } = useContext(CartContext);
 
-    const handleAddToCart = () => {
+	const handleAddToCart = () => {
 		toggleCart();
 	};
 
-    return (
-        <Sheet open={isOpen} onOpenChange={toggleCart}>
-				<SheetContent>
-					<SheetHeader>
-						<SheetTitle>Title</SheetTitle>
-						<SheetDescription>
-							Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nam
-							necessitatibus est a sequi aut quidem repellat ratione dignissimos
-							molestiae, officiis aliquid magni impedit doloribus praesentium,
-							consectetur eveniet ipsum repellendus facere!
-						</SheetDescription>
-					</SheetHeader>
-				</SheetContent>
-			</Sheet>
-    );
-}
+	return (
+		<Sheet open={isOpen} onOpenChange={toggleCart}>
+			<SheetContent>
+				<SheetHeader>
+					<SheetTitle>Title</SheetTitle>
+					<SheetDescription>
+						Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nam
+						necessitatibus est a sequi aut quidem repellat ratione dignissimos
+						molestiae, officiis aliquid magni impedit doloribus praesentium,
+						consectetur eveniet ipsum repellendus facere!
+					</SheetDescription>
+				</SheetHeader>
+				{products.map((product) => (
+					<h1 key={product.id}>
+						{product.name} - {product.quantity}
+					</h1>
+				))}
+			</SheetContent>
+		</Sheet>
+	);
+};
 
 export default CartSheet;
