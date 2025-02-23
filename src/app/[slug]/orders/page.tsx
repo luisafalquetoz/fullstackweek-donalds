d42ba91c54ad312f85b99f88c10020453c9b489a
luisafalquetoz <luisa_fzandonade@hotmail.com>
2025-02-23 17:11:06 -0300

    feat: ensure cart summary is shown on restaurant page

--- src/app/[slug]/orders/page.tsx
+++ src/app/[slug]/orders/page.tsx
@@ -1,45 +1,43 @@
-import { db } from "@/lib/prisma";
-import { isValidCPF, removeCpfPunctuation } from "../menu/helpers/cpf";
-import CpfForm from "./components/cpf-form";
-import OrderList from "./components/order-list";
+import { db } from '@/lib/prisma';
+import { isValidCPF, removeCpfPunctuation } from '../menu/helpers/cpf';
+import CpfForm from './components/cpf-form';
+import OrderList from './components/order-list';
 
 interface OrdersPageProps {
-    searchParams: Promise<{cpf: string}>
+	searchParams: Promise<{ cpf: string }>;
 }
 
-const OrdersPage = async ({searchParams}: OrdersPageProps) => {
-    const {cpf} = await searchParams;
-    if (!cpf) {
-        return <CpfForm />
-    }
-    if (!isValidCPF(cpf)) {
-        return <CpfForm />
-    }
-    const orders = await db.order.findMany({
-        orderBy: {
-            createdAt: 'desc',
-        },
-        where: {
-            customerCpf: removeCpfPunctuation(cpf)
-        },
-        include: {
-            restaurant: {
-                select: {
-                    name: true,
-                    avatarImageUrl: true,
-                }
-            },
-            orderProducts: {
-                include: {
-                    product: true,
-                }
-            }
-        }
-    })
+const OrdersPage = async ({ searchParams }: OrdersPageProps) => {
+	const { cpf } = await searchParams;
+	if (!cpf) {
+		return <CpfForm />;
+	}
+	if (!isValidCPF(cpf)) {
+		return <CpfForm />;
+	}
+	const orders = await db.order.findMany({
+		orderBy: {
+			createdAt: 'desc',
+		},
+		where: {
+			customerCpf: removeCpfPunctuation(cpf),
+		},
+		include: {
+			restaurant: {
+				select: {
+					name: true,
+					avatarImageUrl: true,
+				},
+			},
+			orderProducts: {
+				include: {
+					product: true,
+				},
+			},
+		},
+	});
 
-    return (
-        <OrderList orders={orders} /> 
-    );
-}
+	return <OrderList orders={orders} />;
+};
 
 export default OrdersPage;
